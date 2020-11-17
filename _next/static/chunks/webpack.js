870dec394588ef64e551d43ec3f7ab1eaf75b171
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-11-16 20:08:00 -0500

    Deploy Next.js to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d65a1aa5828371eec9af";
+/******/ 	var hotCurrentHash = "a247421747d8195aab98";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

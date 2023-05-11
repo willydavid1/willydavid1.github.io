b0825b1d22cca83dcc741a16b466c7e546ed1a2c
willy <willydaconceicao@habi.co>
2023-05-10 22:33:09 -0500

    Add WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        opensans: ["var(--font-opensans)"],
       },
+    },
+    colors: {
+      ...colors,
+      transparent: "transparent",
+      current: "currentColor",
+      main: "#1E1E1E",
+      secondary: "#414141",
+      complimentaryOne: "#A8CABA",
+      complimentaryTwo: "#C2C2A3",
     },
   },
   plugins: [],
-}
+};

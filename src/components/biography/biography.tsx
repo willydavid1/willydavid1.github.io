113a5902fe419c6fa8235a92297d4b5475bd0e17
Willy <willydavidlozada@gmail.com>
2024-12-28 22:44:57 +0100

    Add new bio

--- src/components/biography/biography.tsx
+++ src/components/biography/biography.tsx
@@ -37,21 +37,21 @@ const Biography = () => {
             <div className="col-span-12 space-y-12 relative sm:px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700">
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-400">
                 <p className="">
-                  A los 16 años, creó su primera empresa: Una compañía que se
+                  A los 16 años, creó su primera empresa: una compañía que se
                   encargaba de reciclar aparatos electrónicos como computadoras
                   y celulares que se iban a desechar. Fue un negocio ambiental
                   que desarrolló con la ayuda de sus padres. La innovación
-                  estuvo en usar tecnología para incentivar a las
-                  empresas/personas a darle un buen manejo a sus equipos. (2
-                  años después cerró por la situación del país). Logró tener un
-                  equipo de más de 3 trabajadores y más de 500 contactos,
-                  logrando reciclar más de 2800 computadoras, ayudando al medio
-                  ambiente.
+                  residía en usar tecnología para incentivar a las empresas y a
+                  las personas a darle un buen manejo a sus equipos. Dos años
+                  después, cerró debido a la situación del país. Logró tener un
+                  equipo de más de tres trabajadores y más de 500 contactos,
+                  reciclando más de 2800 computadoras y contribuyendo al cuidado
+                  del medio ambiente.
                 </p>
               </div>
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-400">
-                <p className="">
-                  A los 18 años, Fue becado por
+                <p>
+                  A los 18 años, fue becado por{" "}
                   <a
                     href="https://platzi.com/blog/becas-platzi-facebook/"
                     rel="noopener noreferrer"
@@ -60,11 +60,11 @@ const Biography = () => {
                   >
                     Platzi <HiOutlineExternalLink className="ml-1" />
                   </a>{" "}
-                  y Facebook Developer Circles Program (ahora Meta): Estudio
+                  y Facebook Developer Circles Program (ahora Meta). Estudió
                   tecnología especializándose en temas de desarrollo frontend.
-                  Solo fueron seleccionadas 1000 personas, entre ellas Willy. Si
-                  se puede decir que tuvo un poco de suerte. Le encantó el
-                  código, no solo porque ya tenía un background tecnológico muy
+                  Solo fueron seleccionadas 1000 personas, entre ellas Willy. Se
+                  puede decir que tuvo un poco de suerte. Le encantó el código,
+                  no solo porque ya tenía un conocimiento tecnológico muy
                   básico, sino porque esta beca fue lo que lo motivó a seguir
                   aprendiendo día a día y a tener la oportunidad de entrar de
                   lleno al mundo de la tecnología.
@@ -72,8 +72,8 @@ const Biography = () => {
               </div>
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-400">
                 <p className="">
-                  A los 19 años, consiguio su primer empleo en una startup
-                  llamada
+                  A los 19 años, consiguió su primer empleo en una startup
+                  llamada{" "}
                   <a
                     href="https://simetrik.com"
                     rel="noopener noreferrer"
@@ -81,16 +81,16 @@ const Biography = () => {
                     className="pl-1 text-blue-300 underline items-center justify-center inline-flex"
                   >
                     Simetrik <HiOutlineExternalLink className="ml-1" />
-                  </a>{" "}
-                  Allí aprendió sobre el mundo financiero y logró poner en
-                  práctica todo lo que había aprendido sobre tecnología. ¡Sí!
-                  Empezando en plena pandemia de COVID-19.
+                  </a>
+                  . Allí aprendió sobre el mundo financiero y logró poner en
+                  práctica todo lo que había aprendido sobre tecnología. Sí,
+                  comenzó en plena pandemia de COVID-19.
                 </p>
               </div>
 
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-400">
                 <p className="">
-                  A los 20 años, empezó a trabajar en una startup llamada
+                  A los 20 años, comenzó a trabajar en una startup llamada
                   <a
                     href="https://habi.co"
                     rel="noopener noreferrer"
@@ -102,8 +102,8 @@ const Biography = () => {
                   Logró potenciar su carrera en tecnología en muy poco tiempo.
                   Como dice el dicho, trabajar un año en una startup es como
                   trabajar cinco en una empresa tradicional, y la verdad es que
-                  lo confirmo. En tan solo dos años, aprendí cuatro veces más de
-                  lo que había aprendido en toda mi carrera.
+                  lo confirma. En tan solo dos años, aprendió cuatro veces más
+                  de lo que había aprendido en toda su carrera.
                 </p>
               </div>
 
@@ -111,8 +111,8 @@ const Biography = () => {
                 <div>
                   <p>
                     Le interesaba mucho el tema de desarrollo móvil (apps) y
-                    modelos 3D tanto que en 2 meses aprendio utilizar blender
-                    completo un curso de
+                    modelos 3D, tanto que en 2 meses aprendió a utilizar
+                    Blender, completó un curso de
                     <a
                       href="https://platzi.com/p/willydaconceicao/curso/2789-blender/diploma/detalle/"
                       rel="noopener noreferrer"
@@ -121,8 +121,8 @@ const Biography = () => {
                     >
                       Modelado 3D usando Blender{" "}
                       <HiOutlineExternalLink className="ml-1" />
-                    </a>{" "}
-                    Con ese mismo conocimiento, fundó 3 proyectos:
+                    </a>
+                    . Con ese mismo conocimiento, fundó 3 proyectos:
                   </p>
 
                   <ul className="list-disc pl-12 pt-1">
@@ -144,7 +144,7 @@ const Biography = () => {
                       <b>Koalas TV:</b> Fue un proyecto NFT creado con Blender
                       para crear 888 koalas únicos dentro de un televisor con
                       distintas variaciones. Esta vez se inspiró en un regalo
-                      físico que quiso plasmarlo en un mundo virtual.
+                      físico que quiso plasmar en un mundo virtual.
                       <a
                         href="https://opensea.io/collection/koalastv/offers"
                         rel="noopener noreferrer"
@@ -156,12 +156,12 @@ const Biography = () => {
                       </a>
                     </li>
                     <li>
-                      <b>Foodko:</b> En el 2022 fundé esta startup. Foodko era
-                      la comida del futuro; podías ordenar comida utilizando
-                      realidad aumentada y realidad virtual. Innovamos y
-                      mejoramos la experiencia del usuario con una entrega en
+                      <b>Foodko:</b> En el 2022 fundó esta startup. Foodko era
+                      la comida del futuro; se podía ordenar comida utilizando
+                      realidad aumentada y realidad virtual. Innovaron y
+                      mejoraron la experiencia del usuario con una entrega en
                       minutos, cambiando la forma de hacer pedidos. (Foodko
-                      fracasó a los 4 meses) Aprendí que armar un cohete
+                      fracasó a los 4 meses) Aprendió que armar un cohete
                       espacial sin antes validar la idea
                       <a
                         href="https://neat-maple-aedd3.netlify.app/"
@@ -178,8 +178,8 @@ const Biography = () => {
               </div>
 
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-blue-400">
-                <p className="">
-                  A los 21 años, fundo
+                <p>
+                  A los 21 años, fundó
                   <a
                     href="https://picassoia.com"
                     rel="noopener noreferrer"
@@ -192,7 +192,7 @@ const Biography = () => {
                   una sola app. Fueron pioneros en introducir modelos de IA en
                   LATAM. Después de muchos fracasos, incluyendo a Saxior (una
                   startup que buscaba vender ropa con IA), Picasso IA sí fue un
-                  proyecto que tuvo éxito. Logrando más de 110,000 descargas en
+                  proyecto que tuvo éxito. Logró más de 110,000 descargas en
                   <a
                     href="https://play.google.com/store/apps/details?id=com.picassoia.app"
                     rel="noopener noreferrer"
